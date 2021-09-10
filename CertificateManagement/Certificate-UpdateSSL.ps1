63309ac754ff0dd743561381ad1b2ab4ff1019ba
viOpsGit <90460936+viOpsGit@users.noreply.github.com>
2021-09-10 13:41:29 -0400

    Update Certificate-UpdateSSL.ps1

--- CertificateManagement/Certificate-UpdateSSL.ps1
+++ CertificateManagement/Certificate-UpdateSSL.ps1
@@ -2,7 +2,7 @@ Import-Module WebAdministration
 
 ##get all current site bindings
 $Bindings=get-item IIS:\SslBindings\* | where {$_.port -eq 443}
-$Certificate=Get-ChildItem Cert:\LocalMachine\My | Where {$_.FriendlyName -like "*2021*"}
+$Certificate=Get-ChildItem Cert:\LocalMachine\My | Where {$_.FriendlyName -like "Wildcard*2021*"}
 
 
 ForEach($Binding in $Bindings){
