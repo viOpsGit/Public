4eeea139e70a174e3f88b8d3e0a81261732a66b3
viOpsGit <90460936+viOpsGit@users.noreply.github.com>
2021-09-13 13:03:12 -0400

    Revert "Update Certificate-UpdateSSL.ps1"
    This reverts commit bb3ffefa3a3cd6b0a84dac82eb1f4bb1adc935b6.

--- CertificateManagement/Certificate-UpdateSSL.ps1
+++ CertificateManagement/Certificate-UpdateSSL.ps1
@@ -1,13 +1,14 @@
-#!/bin/bash
+Import-Module WebAdministration
 
-
+##get all current site bindings
 $Bindings=get-item IIS:\SslBindings\* | where {$_.port -eq 443}
 $Certificate=Get-ChildItem Cert:\LocalMachine\My | Where {$_.FriendlyName -like "Wildcard*2021*"}
 
+
 ForEach($Binding in $Bindings){
 
-  $Binding | Remove-Item
+$Binding | Remove-Item -Force
 
-  $certificate | New-Item -path "IIS:\SslBindings\$($binding.IPAddress)!$($binding.Port)"
+$certificate | New-Item -path "IIS:\SslBindings\$($binding.IPAddress)!$($binding.Port)" -Force
 
 }
