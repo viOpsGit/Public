4195a13a1006bc8f52330804c395f2e2c9b6da37
viOpsGit <90460936+viOpsGit@users.noreply.github.com>
2021-10-06 12:14:47 -0400

    Add permissions to IIS_IUSRS
    Add permisssions to IIS_IUSRS
    add some error catching and logging

--- CertificateManagement/Certificate-UpdateSSL.ps1
+++ CertificateManagement/Certificate-UpdateSSL.ps1
@@ -1,14 +1,56 @@
 Import-Module WebAdministration
 
-##get all current site bindings
-$Bindings=get-item IIS:\SslBindings\* | where {$_.port -eq 443}
-$Certificate=Get-ChildItem Cert:\LocalMachine\My | Where {$_.FriendlyName -like "Wildcard*2021*"}
+#Set log file
+$logfile = "E:\Sites\CertificateUpdate.txt"
 
+#Get Certificate details
+$Certificate = Get-ChildItem Cert:\LocalMachine\My | Where { $_.FriendlyName -like "Wildcard*2021*" }
 
-ForEach($Binding in $Bindings){
+##Add Permissions to Private Key for built in group IIS_IUSRs
+Try {
 
-$Binding | Remove-Item -Force
+    $rsaCert = [System.Security.Cryptography.X509Certificates.RSACertificateExtensions]::GetRSAPrivateKey($certificate)
+    $fileName = $rsaCert.key.UniqueName
 
-$certificate | New-Item -path "IIS:\SslBindings\$($binding.IPAddress)!$($binding.Port)" -Force
+    If (Test-Path -Path "$env:ALLUSERSPROFILE\Microsoft\Crypto\RSA\MachineKeys\$fileName") {
+        $path = "$env:ALLUSERSPROFILE\Microsoft\Crypto\RSA\MachineKeys\$fileName"
+    }
+    else {
+        $path = Get-ChildItem -recurse -filter $filename -Path "$env:ALLUSERSPROFILE\Microsoft\Crypto\"
+        $path = $path.FullName
+    }
+
+    $permissions = Get-Acl -Path $path
+    $access_rule = New-Object System.Security.AccessControl.FileSystemAccessRule("IIS_IUSRS", 'Read', 'None', 'None', 'Allow')
+    $permissions.AddAccessRule($access_rule)
+    Set-Acl -Path $path -AclObject $permissions
+
+    $logmessage = "Successfully added read permissions to private key for IIS_IUSRS"
+    $logmessage >> $logfile
 
 }
+Catch {
+
+    $logmessage = "Unable to add read permissions to private key for IIS_IUSRS"
+    $logmessage >> $logfile
+    $_ >> $logfile
+}
+
+##Get and replace all site bindings with the new certificate
+
+Try{
+    $Bindings = get-item IIS:\SslBindings\* | where { $_.port -eq 443 }
+
+ForEach ($Binding in $Bindings) {
+
+    $Binding | Remove-Item -Force
+    $certificate | New-Item -path "IIS:\SslBindings\$($binding.IPAddress)!$($binding.Port)" -Force
+}
+}
+Catch{
+    $logmessage = "Unable to add read permissions to private key for IIS_IUSRS"
+    $logmessage >> $logfile
+    $_ >> $logfile
+}
+
+
