ea0e3d6a242fbd3c4bb7161fff0720703aa958cd
viOpsGit <90460936+viOpsGit@users.noreply.github.com>
2021-10-12 16:30:18 -0400

    Update Certificate-UpdateSSLwSSO.ps1
    Update web.config

--- CertificateManagement/Certificate-UpdateSSLwSSO.ps1
+++ CertificateManagement/Certificate-UpdateSSLwSSO.ps1
@@ -60,96 +60,31 @@ Catch{
     $_ >> $logfile
 }
 
-###
-### Abdalla - Update thumbprint in web.config file
-###
+###Update Certificate thumbprint
+$filelocs = @( Get-ChildItem -recurse -filter "web.config" -Path "E:\Sites\")
 
-####set variables here
-$webconfigname = "Web.config"
-
-<##### File location needs to searched
-
-Something like $filelocs=@( Get-ChildItem -recurse -filter $webconfigname -Path "E:\Sites\")
-Then a ForEach ($file in $filelocs) {Run the script and replace the thumbprint}
-
-###>
-
-#####Start Loop
-do {
-		#Write-Host -NoNewline " `n`nChecking Folder "
-		#Write-Host -NoNewline " `n`nWhere is webconfig located?  "
-		#$filloc = Read-Host "Please enter the folder where webconfig is located"
-		
-		#### check that the folder does exist, if so set it and find web config
-		$pathexists =  ( $(Try { Test-Path $filloc.trim() } Catch { $false }) )
-
-		#### folder path exists
-		if ($pathexists -eq ($true)){
+ForEach ($file in $filelocs) {			
 				
-				#### path ok find the webconfig
-				#write-host "Path OK"
-				$filexists = ( $(Try { Test-Path ($filloc.trim() + "\" +$webconfigname.trim()) } Catch { $false }) ) 
-					
-					#### File found
-					if ($filexists -eq $true) {
-						#write-host "File Found"
-						
-						###set the file 
-						$file = ($filloc.trim() + "\" +$webconfigname.trim())
-						
-						#### grab as xml
-						## $xml = Get-Content $filexists -as [Xml]  ###another way of writting it bellow
-						$xml = [xml](Get-Content $file)
+	#### grab as xml
+	$xml = [xml](Get-Content $file.FullName)
 												
-						###create array with what to look for and what to change to 
-						$Dictionary = @{
-							"LocalCertificateSerialNumber"    = $Certificate.Thumbprint
-			
-						}
+	###create array with what to look for and what to change to 
+	$Dictionary = @{
+		"LocalCertificateSerialNumber" = $Certificate.SerialNumber			
+	}
 
-						foreach($key in $Dictionary.Keys)
-						{
-							#Write-Host "Locating key: '$key' in XML"
-							######Use XPath to find the appropriate node
-							if(($addKey = $xml.SelectSingleNode("//appSettings/add[@key = '$key']")))
-							{
-								#Write-Host "Found key: '$key' in XML, updating value to $($Dictionary[$key])"
-								$addKey.SetAttribute('value',$Dictionary[$key])
-							}
+	foreach ($key in $Dictionary.Keys) {
+		######Use XPath to find the appropriate node
+		if (($addKey = $xml.SelectSingleNode("//appSettings/add[@key = '$key']"))) {
+
+			$addKey.SetAttribute('value', $Dictionary[$key])
+
+			$logmessage = "Web Config files completed successfully for $($file.fullname) and $key" 
+			$logmessage >> $logfile
+			$_ >> $logfile
+		}
 							
-							####save changes
-							$xml.Save($file)
-						}
-						
-						####Everything complete exit 
-						#Write-Host "`nComplete!"
-						#Read-Host "(Press any key to exit)"
-                        $logmessage = "Web Config files completed successfully" 
-                        $logmessage >> $logfile
-                        $_ >> $logfile
-						Exit
-					}
-				
-				#### path ok but file not found
-				ELSE {
-					#Write-Host "File Not Found "  ($filloc.trim() + "\" +$webconfigname.trim()) 
-					#Read-Host "(Press any key to exit)"
-                    $logmessage = "File Not Found " + ($filloc.trim() + "\" +$webconfigname.trim()) 
-                    $logmessage >> $logfile
-                    $_ >> $logfile
-					Exit
-				}
-		}
-		
-
-	#### path not found
-	Else {
-	   #write-host "Path not found"
-       $logmessage = "Path not found"
-       $logmessage >> $logfile
-       $_ >> $logfile
-		}
+		####save changes
+		$xml.Save($file)
+	}					
 }
-
-###end Loop
-While($pathexists -eq $false)
